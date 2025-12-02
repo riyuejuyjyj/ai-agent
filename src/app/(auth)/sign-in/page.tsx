2e19781f6ceb5d5d84c18c54ba3da3b66193cc2a
tongzhou <tongzhou@tongzhoudeMacBook-Pro.local>
2025-12-02 20:37:35 +0800

    04:add auth-ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import SignInView from "@/components/auth/ui/views/SignInView";
+
+const SignInPage = () => {
+  return <SignInView />;
+};
+
+export default SignInPage;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import SignUpView from "@/components/auth/ui/views/SignUpView";
+
+const SignUpPage = () => {
+  return <SignUpView></SignUpView>;
+};
+
+export default SignUpPage;
