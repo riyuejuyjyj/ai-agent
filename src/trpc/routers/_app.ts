228bd280d178a0dbdd90e64a284cddc1d2261f91
tongzhou <tongzhou@tongzhoudeMacBook-Pro.local>
2026-03-11 10:20:57 +0800

    16:添加meeting基础设置

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { agentsRouter } from "@/components/agents/server/procedure";
 import { createTRPCRouter } from "../init";
+import { meetingsRouter } from "@/components/meetings/server/procedure";
 
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
+  meeting: meetingsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
