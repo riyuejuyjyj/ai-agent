b93d9ff798ea068be122f38ff7946f88e1f030ad
tongzhou <tongzhou@tongzhoudeMacBook-Pro.local>
2026-03-18 10:08:03 +0800

    18:添加meetingDataTable

--- src/components/agents/types.ts
+++ src/components/agents/types.ts
@@ -21,3 +21,6 @@ import { inferRouterOutputs } from "@trpc/server";
  * }
  */
 export type AgentGetOne = inferRouterOutputs<AppRouter>["agents"]["getOne"];
+
+export type AgentGetMany =
+  inferRouterOutputs<AppRouter>["agents"]["getMany"]["items"];
